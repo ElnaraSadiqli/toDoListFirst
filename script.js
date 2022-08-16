5e823712277a001749c39528c4534bdde2dab9b1
ElnaraSadiqli <elnaragk@code.edu.az>
2022-08-16 14:07:20 +0400

    Add,DElete,Update

--- script.js
+++ script.js
@@ -13,11 +13,12 @@ let inPut = document.querySelector("#todoInput");
 let ul = document.querySelector(".card-body-list");
 
 let dataList = [];
-
+let isEditTask = false;
+let editId;
 
 eventListeners();
+displayList(e);
 
-displayList();
 
 function eventListeners() {
     inputIcon.addEventListener("click", () => {
@@ -55,65 +56,116 @@ function displayInput(event) {
 
 // --------------------Add Task 
 
-
 // Display Task on List 
 function displayList(e) {
 
-    let li;
+    ul.innerHTML = "";
 
     for (let data of dataList) {
 
-        dataList = [];
 
-        li = `<li class="d-flex  justify-content-around align-items-center">
-        <form class="card-form-check">
-           <input type="checkbox"  id="${data.id}" class="inputName input-check">
-           <label class="card-form-label" for="${data.id}">${data.name}</label>
-       </form>
-   
-       <div class="dropdown dropdown-checkbox">
-           <button class="btn dropdown-toggle" id="dropdown-btn" type="button"
-               data-toggle="dropdown" aria-expanded="false">
-               <i class="fa-solid fa-ellipsis"></i>
-           </button>
-           <div class="dropdown-menu">
-               <a class="dropdown-item" href="#">
-                   <i class="fa-solid fa-circle-minus"></i>
-               </a>
-               <a class="dropdown-item" href="#">
-                   <i class="fa-regular fa-pen-to-square"></i>
-   
-               </a>
-           </div>
-           </div>
-           </li>`
+        let li = `<li class="d-flex  justify-content-around mt-4 align-items-center">
+            <form class="card-form-check">
+               <input type="checkbox"  id="${data.id}" class="inputName input-check">
+               <label class="card-form-label" for="${data.id}">${data.name}</label>
+           </form>
+       
+           <div class="dropdown dropdown-checkbox">
+               <button class="btn dropdown-toggle"  type="button" id="dropdown-btn" 
+                   data-toggle="dropdown" aria-expanded="false">
+                   <i class="fa-solid fa-ellipsis"></i>
+               </button>
+               <div class="dropdown-menu">
+                   <a class="dropdown-item" href="#" onclick="deleteTask(${data.id})" >
+                       <i class="fa-solid fa-circle-minus"></i>
+                   </a>
+                   <a class="dropdown-item" href="#" onclick="updateTask(${data.id},'${data.name}')">
+                       <i class="fa-regular fa-pen-to-square"></i>
+       
+                   </a>
+               </div>
+               </div>
+               </li>`
 
 
         ul.insertAdjacentHTML("beforeend", li);
 
+    }
+    inPut.value = "";
 
+    e.preventDefault();
+
+}
+// List Display on Screen 
+function newTask(e) {
+
+
+    if (inPut.value == "") {
+
+        alert("Please input task,don't stop value")
+    } else {
+        if (!isEditTask) {
+            // Add Task
+            dataList.push({
+                "id": dataList.length + 1,
+                "name": inPut.value,
+                "status": "complated",
+            })
+
+        } else {
+            // Update Task
+            addUpdatingTask();
+        }
 
     }
-
-
+    displayList();
 
     e.preventDefault();
 
 }
 
-// List Display on Screen 
-function newTask(e) {
+// --------------------Delete Task 
+function deleteTask(item) {
 
-    e.preventDefault();
+    let id;
 
-    dataList.push({
-        "id": dataList.length + 1,
-        "name": inPut.value,
-        "status": "complated",
+
+    id = dataList.findIndex((task) => {
+        task.id == item;
     })
 
+    dataList.splice(id, 1);
 
     displayList();
+}
+// --------------------Update Task 
+function updateTask(taskId, taskName) {
+    //    Equal parametr
+    editId = taskId;
+
+    isEditTask = true;
+
+    inputCard.style.display = "block";
+
+    inPut.value = taskName;
+
+    inPut.focus();
+
+    inPut.classList.add("active");
 
 
+
+}
+
+function addUpdatingTask() {
+
+    for (let data of dataList) {
+       
+        if (data.id == editId) {
+         
+            data.name = inPut.value;
+        }
+       
+        isEditTask = false;
+    }
 }
